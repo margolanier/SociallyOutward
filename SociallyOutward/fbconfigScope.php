033cdb5615a3427c11b44f2351f757f09b374d7b
margolanier <mplanier@live.unc.edu>
2014-05-22 20:30:28 -0400

    original calendar

--- SociallyOutward/fbconfigScope.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require '../fbconfig.php';
-
-echo '<script>console.log(' . $user . ');</script>';
-
-?>
--- SociallyOutward/setCookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'fbconfig.php';
-require 'getUser.php';
-
-$UID = getUIDFromFBID($fbid,$connection);
-setcookie('user', $UID, time()+60*60*24*30, '/', "sociallyoutward.com", false);
-header("location: memberProfile.php");
-
-?>
